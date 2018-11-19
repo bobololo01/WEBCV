99bcf9128bddb782b88ea03ff5391ae66e25a1b1
Vinh <apple@Apples-MacBook-2.local>
2018-11-19 09:25:24 +0700

    19/11/2018

---
+++ src/app/modules/resumes/resumes.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from "@angular/core";
+import { ResumesRouteModule } from "./resumes.route";
+
+@NgModule({
+    imports: [
+        ResumesRouteModule
+    ],
+    declarations: [
+
+    ],
+    exports: [
+
+    ]
+})
+
+export class ResumesModule {
+    
+}
---
+++ src/app/modules/resumes/resumes.route.ts
@@ -0,0 +1,17 @@
+import { Routes, RouterModule } from "@angular/router";
+import { NgModule } from "@angular/core";
+
+const route: Routes = [
+    {
+        
+    }
+];
+
+@NgModule({
+    imports: [RouterModule.forChild(route)],
+    exports: [RouterModule]
+})
+
+export class ResumesRouteModule {
+    
+}
